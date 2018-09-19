ad8b1263d6ce50eee8a6a899280423c095c06910
Matous Synek <xsynek3@fi.muni.cz>
2018-09-19 22:57:04 +0200

    mini1 99,11

--- miniukol1/tests.cpp
+++ miniukol1/tests.cpp
@@ -11,4 +11,24 @@ TEST_CASE("Email address tests") {
 	CHECK(!isEmailAddress("vladimir@"));
 	CHECK(!isEmailAddress("honza@moje_domena.cz"));
 	CHECK(isEmailAddress("jan@123.cz"));
+	CHECK(!isEmailAddress("aaa@a a.aa"));
+	CHECK(!isEmailAddress("aa\\a@aa.aa"));
+	CHECK(!isEmailAddress("aa@aa\\.aa"));
+	CHECK(!isEmailAddress("a@.."));
+	CHECK(!isEmailAddress("A@aa.?"));
+	CHECK(!isEmailAddress(""));
+	CHECK(isEmailAddress("a@a"));
+	CHECK(!isEmailAddress("a@-"));
+	CHECK(!isEmailAddress("a@a-"));
+	CHECK(!isEmailAddress(""));
+	CHECK(!isEmailAddress(".a@aa.aa"));
+	CHECK(!isEmailAddress("a..@aa.aa"));
+	CHECK(!isEmailAddress("A@ .aa"));
+	CHECK(!isEmailAddress("@aa.aa"));
+	CHECK(!isEmailAddress("A@a.5"));
+	CHECK(!isEmailAddress("A@.aa"));
+	CHECK(isEmailAddress("a\\ a@aa.aa"));
+	CHECK(!isEmailAddress("A\\\\ @aa.aa"));
+	CHECK(isEmailAddress("a\\\\\\ @aa.aa"));
+	CHECK(!isEmailAddress("A\\\\\\@aa.aa"));
 }
